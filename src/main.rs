249f06892c536db0a153e1bc0b40decf862d44cd
papojari <papojari-git.ovoid@aleeas.com>
2022-02-26 13:27:27 +0100

    feat: add basic funtionality to print one column of colors

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use clap::Parser;
+use colorful::Colorful;
+use colorful::Color;
+
+#[derive(Parser, Debug)]
+#[clap(name = "sanctity", version, about="ruSt ANsi16 Color Test utIliTY")]
+struct Args {
+    /// Text that will be used to test the colors.
+    #[clap(long, short, default_value = " ••• ")]
+    text: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let text = args.text;
+
+    let ansi16 = [
+        (Color::Black),
+        (Color::DarkGray),
+        (Color::Red),
+        (Color::LightRed),
+        (Color::Green),
+        (Color::LightGreen),
+        (Color::Yellow),
+        (Color::LightYellow),
+        (Color::Blue),
+        (Color::LightBlue),
+        (Color::Magenta),
+        (Color::LightMagenta),
+        (Color::Cyan),
+        (Color::LightCyan),
+        (Color::LightGray),
+        (Color::White),
+    ];
+
+    for color in ansi16 {
+        println!("{}", text.clone().color(color));
+    };
+}
